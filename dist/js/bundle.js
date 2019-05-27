2b9e7b03062b828696c4e287671a3ed6f8861a9b
Alexander Demidenko <alexfoxref@gmail.com>
2019-05-28 03:09:24 +0500

    another download

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -761,12 +761,33 @@ var download = function download() {
       for (var i = 0; i < downloadBtns.length; i++) {
         for (var j = 0; j < downloadBtns[i].querySelectorAll('*').length; j++) {
           if (event.target == downloadBtns[i] || event.target == downloadBtns[i].querySelectorAll('*')[j]) {
-            var link = document.createElement('a');
-            link.setAttribute('href', './dist/download/ECMAScript.pdf');
-            link.setAttribute('download', '');
-            link.click();
-            link.remove();
-            break;
+            var _ret = function () {
+              var downloadFrame = document.createElement('iframe');
+              document.body.appendChild(downloadFrame);
+              downloadFrame.style.position = 'fixed';
+              downloadFrame.style.width = '100%';
+              downloadFrame.style.height = '100%';
+              downloadFrame.style.top = '0';
+              downloadFrame.style.left = '0';
+              downloadFrame.style.zIndex = '40';
+              downloadFrame.setAttribute('src', './dist/download/ECMAScript.pdf');
+              var downloadClose = document.createElement('div');
+              document.body.appendChild(downloadClose);
+              downloadClose.innerHTML = "&times;";
+              downloadClose.classList.add('close__pdf');
+              downloadClose.style.zIndex = '50';
+
+              var closePdf = function closePdf() {
+                document.body.removeChild(downloadFrame);
+                downloadClose.removeEventListener('click', closePdf);
+                document.body.removeChild(downloadClose);
+              };
+
+              downloadClose.addEventListener('click', closePdf);
+              return "break";
+            }();
+
+            if (_ret === "break") break;
           }
         }
       }
--- src/js/parts/download.js
+++ src/js/parts/download.js
@@ -7,11 +7,26 @@ let download = () => {
             for (let i = 0; i < downloadBtns.length; i++) {
                 for (let j = 0; j < downloadBtns[i].querySelectorAll('*').length; j++) {
                     if (event.target == downloadBtns[i] || event.target == downloadBtns[i].querySelectorAll('*')[j]) {
-                        let link = document.createElement('a');
-                        link.setAttribute('href', './dist/download/ECMAScript.pdf');
-                        link.setAttribute('download', '');
-                        link.click();
-                        link.remove();
+                        let downloadFrame = document.createElement('iframe');
+                        document.body.appendChild(downloadFrame);
+                        downloadFrame.style.position = 'fixed';
+                        downloadFrame.style.width = '100%';
+                        downloadFrame.style.height = '100%';
+                        downloadFrame.style.top = '0';
+                        downloadFrame.style.left = '0';
+                        downloadFrame.style.zIndex = '40';
+                        downloadFrame.setAttribute('src', './dist/download/ECMAScript.pdf');
+                        let downloadClose = document.createElement('div');
+                        document.body.appendChild(downloadClose);
+                        downloadClose.innerHTML = `&times;`;
+                        downloadClose.classList.add('close__pdf');
+                        downloadClose.style.zIndex = '50';
+                        let closePdf = () => {
+                            document.body.removeChild(downloadFrame);
+                            downloadClose.removeEventListener('click', closePdf);
+                            document.body.removeChild(downloadClose);
+                        }
+                        downloadClose.addEventListener('click', closePdf);
                         break;
                     }
                 }
