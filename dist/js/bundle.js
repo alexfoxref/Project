981c395f269b12dc1cff708ddfa3ff57e2c2bc9a
Alexander Demidenko <alexfoxref@gmail.com>
2019-05-28 14:05:16 +0500

    download php

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -761,33 +761,28 @@ var download = function download() {
       for (var i = 0; i < downloadBtns.length; i++) {
         for (var j = 0; j < downloadBtns[i].querySelectorAll('*').length; j++) {
           if (event.target == downloadBtns[i] || event.target == downloadBtns[i].querySelectorAll('*')[j]) {
-            var _ret = function () {
-              var downloadFrame = document.createElement('iframe');
-              document.body.appendChild(downloadFrame);
-              downloadFrame.style.position = 'fixed';
-              downloadFrame.style.width = '100%';
-              downloadFrame.style.height = '100%';
-              downloadFrame.style.top = '0';
-              downloadFrame.style.left = '0';
-              downloadFrame.style.zIndex = '40';
-              downloadFrame.setAttribute('src', './dist/download/ECMAScript.pdf');
-              var downloadClose = document.createElement('div');
-              document.body.appendChild(downloadClose);
-              downloadClose.innerHTML = "&times;";
-              downloadClose.classList.add('close__pdf');
-              downloadClose.style.zIndex = '50';
-
-              var closePdf = function closePdf() {
-                document.body.removeChild(downloadFrame);
-                downloadClose.removeEventListener('click', closePdf);
-                document.body.removeChild(downloadClose);
-              };
-
-              downloadClose.addEventListener('click', closePdf);
-              return "break";
-            }();
-
-            if (_ret === "break") break;
+            // let downloadFrame = document.createElement('iframe');
+            // document.body.appendChild(downloadFrame);
+            // downloadFrame.style.position = 'fixed';
+            // downloadFrame.style.width = '100%';
+            // downloadFrame.style.height = '100%';
+            // downloadFrame.style.top = '0';
+            // downloadFrame.style.left = '0';
+            // downloadFrame.style.zIndex = '40';
+            // downloadFrame.setAttribute('src', './dist/download/ECMAScript.pdf');
+            // let downloadClose = document.createElement('div');
+            // document.body.appendChild(downloadClose);
+            // downloadClose.innerHTML = `&times;`;
+            // downloadClose.classList.add('close__pdf');
+            // downloadClose.style.zIndex = '50';
+            // let closePdf = () => {
+            //     document.body.removeChild(downloadFrame);
+            //     downloadClose.removeEventListener('click', closePdf);
+            //     document.body.removeChild(downloadClose);
+            // }
+            // downloadClose.addEventListener('click', closePdf);
+            window.location.href = 'download.php?filename=./dist/download/ECMAScript.pdf';
+            break;
           }
         }
       }
--- src/js/parts/download.js
+++ src/js/parts/download.js
@@ -7,26 +7,27 @@ let download = () => {
             for (let i = 0; i < downloadBtns.length; i++) {
                 for (let j = 0; j < downloadBtns[i].querySelectorAll('*').length; j++) {
                     if (event.target == downloadBtns[i] || event.target == downloadBtns[i].querySelectorAll('*')[j]) {
-                        let downloadFrame = document.createElement('iframe');
-                        document.body.appendChild(downloadFrame);
-                        downloadFrame.style.position = 'fixed';
-                        downloadFrame.style.width = '100%';
-                        downloadFrame.style.height = '100%';
-                        downloadFrame.style.top = '0';
-                        downloadFrame.style.left = '0';
-                        downloadFrame.style.zIndex = '40';
-                        downloadFrame.setAttribute('src', './dist/download/ECMAScript.pdf');
-                        let downloadClose = document.createElement('div');
-                        document.body.appendChild(downloadClose);
-                        downloadClose.innerHTML = `&times;`;
-                        downloadClose.classList.add('close__pdf');
-                        downloadClose.style.zIndex = '50';
-                        let closePdf = () => {
-                            document.body.removeChild(downloadFrame);
-                            downloadClose.removeEventListener('click', closePdf);
-                            document.body.removeChild(downloadClose);
-                        }
-                        downloadClose.addEventListener('click', closePdf);
+                        // let downloadFrame = document.createElement('iframe');
+                        // document.body.appendChild(downloadFrame);
+                        // downloadFrame.style.position = 'fixed';
+                        // downloadFrame.style.width = '100%';
+                        // downloadFrame.style.height = '100%';
+                        // downloadFrame.style.top = '0';
+                        // downloadFrame.style.left = '0';
+                        // downloadFrame.style.zIndex = '40';
+                        // downloadFrame.setAttribute('src', './dist/download/ECMAScript.pdf');
+                        // let downloadClose = document.createElement('div');
+                        // document.body.appendChild(downloadClose);
+                        // downloadClose.innerHTML = `&times;`;
+                        // downloadClose.classList.add('close__pdf');
+                        // downloadClose.style.zIndex = '50';
+                        // let closePdf = () => {
+                        //     document.body.removeChild(downloadFrame);
+                        //     downloadClose.removeEventListener('click', closePdf);
+                        //     document.body.removeChild(downloadClose);
+                        // }
+                        // downloadClose.addEventListener('click', closePdf);
+                        window.location.href = 'download.php?filename=./dist/download/ECMAScript.pdf'
                         break;
                     }
                 }
